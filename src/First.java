24f37b36003c3c66f148cd5b8a0b393678f6db9e
Anuj_Sundriyal_Coditas <anujsundriyal1234@gmail.com>
2022-07-19 16:37:43 +0530

    this is first file

---
+++ src/First.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+public class First {
+
+    public static void main(String args[])
+    {
+        int a = 0;
+        for(int i=0;i<6;i++)
+        {
+            a++;
+        }
+        System.out.println(a);
+    }
+
+}
